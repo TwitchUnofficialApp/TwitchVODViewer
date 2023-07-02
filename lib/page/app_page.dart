dd1c4c184ee68c155a7a5aadfbe7961fb3e651fd
raymondhung010 <raymondhung010@gmail.com>
2023-07-02 13:49:57 +0800

    update apps page ui

--- lib/page/app_page.dart
+++ lib/page/app_page.dart
@@ -1,11 +1,61 @@
 import 'package:flutter/material.dart';
-import 'package:fluent_ui/fluent_ui.dart';
 
 class AppsPage extends StatelessWidget {
+
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('Apps Page'),
+    return LayoutBuilder(
+      builder: (BuildContext context, BoxConstraints constraints) {
+        return Center(
+          child: Row(
+            mainAxisSize: MainAxisSize.min,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Expanded(
+                child: Container(
+                  height: constraints.maxHeight,
+                  child: Column(
+                    children: [
+
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          SizedBox(
+                            width: 150,
+                            child: TextField(
+                              decoration: InputDecoration(
+                                hintText: 'Streamer ID',
+                              ),
+                            ),
+                          ),
+                          SizedBox(width: 10),
+                          ElevatedButton(
+                            onPressed:(){},
+                            child: Text('Search')
+                          )
+                        ],
+                      )
+
+                    ]
+                  ),
+                ),
+              ),
+              SizedBox(width: 50),
+              Expanded(
+                child: Container(
+                  height: constraints.maxHeight,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      
+                    ]
+                  ),
+                ),
+              )
+            ],
+          ),
+        );
+      },
     );
   }
 }
